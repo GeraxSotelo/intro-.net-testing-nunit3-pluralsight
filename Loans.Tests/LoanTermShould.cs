0dd7e4004996dbda6781579f0a3d19f73a6c2f7c
Gerax Sotelo <gsotelo23@gmail.com>
2020-03-24 15:12:21 -0600

    Install packages. Write first test.

--- Loans.Tests/LoanTermShould.cs
+++ Loans.Tests/LoanTermShould.cs
@@ -1,10 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
 
 namespace Loans.Tests
 {
-    class LoanTermShould
+    [TestFixture]
+    public class LoanTermShould
     {
+        [Test]
+        public void ReturnTermInMonths()
+        {
+            var sut = new LoanTerm(1);
+
+            Assert.That(sut.ToMonths(), Is.EqualTo(12));
+        }
     }
 }
