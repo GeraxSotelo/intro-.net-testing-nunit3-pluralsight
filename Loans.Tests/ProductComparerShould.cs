0ea8c97fbec88473fa59808aaa5490bd1c0b23f7
Gerax Sotelo <gsotelo23@gmail.com>
2020-03-25 15:19:29 -0600

    Test to check if a list contains an expected product

--- Loans.Tests/ProductComparerShould.cs
+++ Loans.Tests/ProductComparerShould.cs
@@ -33,5 +33,20 @@ namespace Loans.Tests
             // Make sure there are no duplicates
             Assert.That(comparisons, Is.Unique);
         }
+
+        [Test]
+        public void ReturnComparisonForFirstProduct()
+        {
+            var products = new List<LoanProduct> { new LoanProduct(1, "a", 1), new LoanProduct(2, "b", 2), new LoanProduct(3, "c", 3) };
+
+            var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+
+            List<MonthlyRepaymentComparison> comparisons = sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            var expectedProduct = new MonthlyRepaymentComparison("a", 1, 643.28m);
+
+            // Check that the list of comparisons contains the expected loan product (Will need to know the expected monthly repayment)
+            Assert.That(comparisons, Does.Contain(expectedProduct));
+        }
     }
 }
