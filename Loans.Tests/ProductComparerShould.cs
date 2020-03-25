640628516af53b87a7973bc7eefe733f7614767a
Gerax Sotelo <gsotelo23@gmail.com>
2020-03-25 15:08:06 -0600

    Test for duplicate comparisons

--- Loans.Tests/ProductComparerShould.cs
+++ Loans.Tests/ProductComparerShould.cs
@@ -20,5 +20,18 @@ namespace Loans.Tests
             // products has 3 items so this test should pass
             Assert.That(comparisons, Has.Exactly(3).Items);
         }
+
+        [Test]
+        public void NotReturnDuplicateComparisons()
+        {
+            var products = new List<LoanProduct> { new LoanProduct(1, "a", 1), new LoanProduct(2, "b", 2), new LoanProduct(3, "c", 3) };
+
+            var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+
+            List<MonthlyRepaymentComparison> comparisons = sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            // Make sure there are no duplicates
+            Assert.That(comparisons, Is.Unique);
+        }
     }
 }
