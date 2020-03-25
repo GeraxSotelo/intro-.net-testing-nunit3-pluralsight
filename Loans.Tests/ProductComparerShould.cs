857a7c55cca4bfab3aadf7810d6f31b7e75ded1b
Gerax Sotelo <gsotelo23@gmail.com>
2020-03-25 15:28:06 -0600

    Test to check if product is in list without knowing specific values

--- Loans.Tests/ProductComparerShould.cs
+++ Loans.Tests/ProductComparerShould.cs
@@ -48,5 +48,18 @@ namespace Loans.Tests
             // Check that the list of comparisons contains the expected loan product (Will need to know the expected monthly repayment)
             Assert.That(comparisons, Does.Contain(expectedProduct));
         }
+
+        [Test]
+        public void ReturnComparisonForFirstProduct_WithPartialKnownExpectedValues()
+        {
+            var products = new List<LoanProduct> { new LoanProduct(1, "a", 1), new LoanProduct(2, "b", 2), new LoanProduct(3, "c", 3) };
+
+            var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+
+            List<MonthlyRepaymentComparison> comparisons = sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            // assert that the product is in the list, but don't care about the expected monthly repayment value, only that the product is there
+            Assert.That(comparisons, Has.Exactly(1).Property("ProductName").EqualTo("a").And.Property("InterestRate").EqualTo(1).And.Property("MonthlyRepayment").GreaterThan(0));
+        }
     }
 }
