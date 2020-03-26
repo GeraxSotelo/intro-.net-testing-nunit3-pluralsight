5c4991dbae9abe20e8e733959867530f04cc339d
Gerax Sotelo <gsotelo23@gmail.com>
2020-03-26 15:36:55 -0600

    NUnit's OneTimeSetUp & OneTimeTearDown attributes

--- Loans.Tests/ProductComparerShould.cs
+++ Loans.Tests/ProductComparerShould.cs
@@ -10,11 +10,28 @@ namespace Loans.Tests
         private List<LoanProduct> products;
         private ProductComparer sut; //system under test
 
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            // This code gets executed a single time before the first test in the test class executes
+
+            // Simulate long setup init time for this list of products. We assume that this list will not be modified by any tests as this will potentially break other tests (i.e. break test isolation)
+            products = new List<LoanProduct> { new LoanProduct(1, "a", 1), new LoanProduct(2, "b", 2), new LoanProduct(3, "c", 3) };
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            // Run after last test in this test class (fixture) executes. e.g. disposing of shared expensive setup performed in OneTimeSetUp
+
+            // products.Dispose(); e.g. if products implemented IDisposable
+        }
+
         [SetUp]
         public void Setup()
         {
             // This method will get called before each test executes
-            products = new List<LoanProduct> { new LoanProduct(1, "a", 1), new LoanProduct(2, "b", 2), new LoanProduct(3, "c", 3) };
+
             sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
         }
 
