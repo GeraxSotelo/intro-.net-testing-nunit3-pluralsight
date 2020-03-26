da0c55b3deea378c53da2efcfbda6a9e3d511915
Gerax Sotelo <gsotelo23@gmail.com>
2020-03-26 16:24:13 -0600

    Test for correct monthly repayments

---
+++ Loans.Tests/LoanRepaymentCalculatorShould.cs
@@ -0,0 +1,21 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loans.Tests
+{
+    public class LoanRepaymentCalculatorShould
+    {
+        [Test]
+        public void CalculateCorrectMonthlyRepayment()
+        {
+            var sut = new LoanRepaymentCalculator();
+
+            var monthlyPayment = sut.CalculateMonthlyRepayment(new LoanAmount("USD", 200_000), 6.5m, new LoanTerm(30));
+
+            Assert.That(monthlyPayment, Is.EqualTo(1264.14));
+        }
+    }
+}
