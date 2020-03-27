8f8753a32b2456a0b9a71a93d4dfdb42ffba339c
Gerax Sotelo <gsotelo23@gmail.com>
2020-03-27 10:13:21 -0600

    Use NUnit's TestCaseSource attribute to centralize test data and reduce duplicate test code

--- Loans.Tests/LoanRepaymentCalculatorShould.cs
+++ Loans.Tests/LoanRepaymentCalculatorShould.cs
@@ -9,6 +9,7 @@ namespace Loans.Tests
     public class LoanRepaymentCalculatorShould
     {
         [Test]
+        // Disadvantage of TestCase attribute is, can't share test data across multiple test methods or test classes
         [TestCase(200_000, 6.5, 30, 1264.14)]
         [TestCase(200_000, 10, 30, 1755.14)]
         [TestCase(500_000, 10, 30, 4387.86)]
@@ -22,10 +23,33 @@ namespace Loans.Tests
         }
 
         [Test]
+        // Disadvantage of TestCase attribute is, can't share test data across multiple test methods or test classes
         [TestCase(200_000, 6.5, 30, ExpectedResult = 1264.14)]
         [TestCase(200_000, 10, 30, ExpectedResult = 1755.14)]
         [TestCase(500_000, 10, 30, ExpectedResult = 4387.86)]
         public decimal CalculateCorrectMonthlyRepayment_SimplifiedTestCase(decimal principal, decimal interestRate, int termInYears)
+        {
+            var sut = new LoanRepaymentCalculator();
+
+            return sut.CalculateMonthlyRepayment(new LoanAmount("USD", principal), interestRate, new LoanTerm(termInYears));
+        }
+
+        [Test]
+        // param1: type that contains the test data. param2: as a string, the property name that will return the test data
+        [TestCaseSource(typeof(MonthlyRepaymentTestData), "TestCases")]
+        public void CalculateCorrectMonthlyRepayment_Centralized(decimal principal, decimal interestRate, int termInYears, decimal expectedMonthlyPayment)
+        {
+            var sut = new LoanRepaymentCalculator();
+
+            var monthlyPayment = sut.CalculateMonthlyRepayment(new LoanAmount("USD", principal), interestRate, new LoanTerm(termInYears));
+
+            Assert.That(monthlyPayment, Is.EqualTo(expectedMonthlyPayment));
+        }
+
+        [Test]
+        // param1: type that contains the test data. param2: as a string, the property name that will return the test data
+        [TestCaseSource(typeof(MonthlyRepaymentTestDataWithReturn), "TestCases")]
+        public decimal CalculateCorrectMonthlyRepayment_CentralizedWithReturn(decimal principal, decimal interestRate, int termInYears)
         {
             var sut = new LoanRepaymentCalculator();
 
