d820b9ce312b39206eca36af904782830b7cd2b5
Gerax Sotelo <gsotelo23@gmail.com>
2020-03-27 14:23:05 -0600

    Create custom category attribute

---
+++ Loans.Tests/ProductComparisonAttribute.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Loans.Tests
+{
+    // Defining a .Net attribute, specify how the attribute can be used. Use .Net's 'AttributeUsage' attribute
+
+    // This attribute can be used at the method or class level and doesn't allow multiple uses of this attribute
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false)]
+    // Inherit from NUnit's 'CategoryAttribute' base class
+    class ProductComparisonAttribute : CategoryAttribute
+    {
+    }
+}
